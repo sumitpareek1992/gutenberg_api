bf167ff1113a5a5ca12f64f7cc314892c0818adc
Sumit Sharma <sumitsharma@topsinfosolutions.com>
2025-05-22 14:25:27 +0530

    static files

--- books/urls.py
+++ books/urls.py
@@ -18,6 +18,9 @@ from django.urls import path,include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -39,3 +42,6 @@ urlpatterns = [
     # Additional Routes
     path('', include('books_store.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
